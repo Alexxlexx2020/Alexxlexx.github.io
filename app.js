4562402a72f0c8d3de964f078d8021afec4484f0
Alexander <107307695+Alexxlexx2020@users.noreply.github.com>
2022-10-16 22:10:10 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 let massive = [
     [, , , , , , , , , , , ,
-        '<img class = "photo" src="image/Mama.jpg" alt="фото" />'
+        '<img class = "photo" src="image/Mama_1953.jpg" alt="фото" />'
+    ],
+    [, , , , , , , , , , , , , , , , , , ,
+        '<img class = "photo" src="image/DedushkaVI_1924.jpg" alt="фото" />'
     ],
     [],
-    [],
     [, , , ,
-        '<img class = "photo" src="image/Lesha.jpg" alt="фото" />', , , , , , , , , , , , ,
-        '<img class = "photo" src="image/Papa.jpg" alt="фото" />'
+        '<img class = "photo" src="image/Lesha_1982.jpg" alt="фото" />', , , , , ,
+        '<img class = "photo" src="image/Asya_1931.jpg" alt="фото" />', , , , , , ,
+        '<img class = "photo" src="image/Papa_1952.jpg" alt="фото" />'
     ],
     [, ,
         '<img class = "photo" src="image/Vita.jpg" alt="фото" />'
@@ -16,13 +19,13 @@ let massive = [
     ],
     [],
     [,
-        '<img class = "photo" src="image/Sasha.jpg" alt="фото" />'
+        '<img class = "photo" src="image/Sasha_1973.jpg" alt="фото" />'
     ],
     [, , , , , , , , , , , , , , , , , , , , , , , ,
         "<div class = 'text1'><br><br><b>Православие:</b><br><br>—отдание праздника Рождества Пресвятой Богородицы;<br>—память священномученика Автонома, епископа Италийского (313 год);<br>—память преподобного Афанасия Высоцкого, Серпуховского чудотворца (1395 год);<br>—память священномучеников Феодора Лебедева, Иоанна Прудентова, Николая Житова, пресвитеров, мученика Алексия (Ворошина) (1937 год);<br>—воспоминание перенесения мощей праведного Симеона Верхотурского (1704 год);<br>—память преподобного Вассиана Тиксненского (1624 год);<br>—память мученика Иулиана и с ним 40 мучеников (IV век)[6];<br>—память мученика Феодора Александрийского;<br>—память священномученика Корнута, епископа Никомидийского (Иконийского) (249-259 годы)[7];<br>—память мучеников Авадира и Ирая.<hr><b>Именины:</b><br><br>Католические: Аврелия, Амелия.<br>Православные: Семён, Фёдор, Юлиан, Святополк.</div>",
         "<div class = 'text1'><b><br><br>Католическая церковь:</b><br>—память святых бессребреников Космы и Дамиана.<br><b>Православие:</b><br>—память обновления (освящения) храма Воскресения Христова в Иерусалиме (Воскресение словущее) (335 год);<br>—предпразднство Воздвижения Честно́го и Животворящего Креста Господня;<br>—память священномученика Корнилия сотника (I век);<br>—память священномучеников Стефана Костогрыза, Александра Аксёнова, пресвитеров и Николая Васюковича, диакона (1937 год);<br>—память мучеников Кронида, Леонтия и Серапиона (около 237 года);<br>—память мучеников Селевка и Стратоника (III век);<br>—память мучеников Макровия и Гордиана (320 год);<br>—память священномученика Иулиана пресвитера (IV век);<br>—память мучеников Илии, Зотика, Лукиана и Валериана (320 год);<br>—память преподобного Петра в Атрое (IX век);<br>—память великомученицы Кетевани, царицы Кахетинской (1624 год) (Груз.).<hr><b>Именины:</b><br> Православные: Валериан, Илья, Корнелий, Леонтий, Лукьян, Пётр, Юлиан.<br>Католические: Люция, Юстина, Дамиан, Киприан.</div>"
     ],
-    [],
+    [, , , , , , ,'<img class = "photo" src="image/Larisa_1936.jpg" alt="фото" />', , , , , , , , , , , , , , , , , , , , , , , '<img class = "photo" src="image/Zhenya_1929.jpg" alt="фото" />'],
     [],
     [],
 ]
@@ -111,7 +114,7 @@ function myTime(elem) {
         'ноября',
         'декабря'
     ];
-    elem.insertAdjacentHTML("afterbegin", `<div id="clock"><span class="spanH"></span><span>:</span><span class="spanM"></span><span>:</span><span class="spanS"></span><span></span><br><span class="current_date"></span></div>`);
+    elem.insertAdjacentHTML("afterbegin", `<div id="clock"><span class="spanH"></span><span class="spanX">:</span><span class="spanM"></span><span class="spanX">:</span><span class="spanS"></span><span></span><br><span class="current_date"></span></div>`);
 
     function timeCalc() {
         let h1 = new Date().getHours();
@@ -173,8 +176,12 @@ function startCalendar(year) {
             let t1 = "<div class = 'no_info'><br><br><br>*Нет информации по этой дате.*</div>";
             if (massive[+elem1.parentNode.parentNode.parentNode.id.split(".")[1]][elem1.innerText - 1] != undefined) t1 = massive[+elem1.parentNode.parentNode.parentNode.id.split(".")[1]][elem1.innerText - 1];
             elem1.insertAdjacentElement("beforeend", mess);
+            let oldYear = "-";
+            if (t1.indexOf('.jpg') != -1) oldYear = document.getElementById('inputka').value - t1.slice(t1.indexOf('.jpg') - 4, t1.indexOf('.jpg'));
+            if (isNaN(oldYear) || +oldYear < 0) oldYear = "-";
+            console.log(oldYear);
             mess.innerHTML = "";
-            mess.innerHTML = `<div class = "text"><b>${elem1.innerText} ${month2[+elem1.parentNode.parentNode.parentNode.id.split(".")[1]]}  ${year} года</b></div>${t1}`;
+            mess.innerHTML = `<div class = "text"><b>${elem1.innerText} ${month2[+elem1.parentNode.parentNode.parentNode.id.split(".")[1]]}  ${year} года, ${oldYear}</b></div>${t1}`;
             mess.insertAdjacentElement('afterbegin', closer_point);
             if (document.body.clientWidth >= document.body.clientHeight) {
                 mess.classList.remove("mess_tel");
@@ -202,7 +209,7 @@ function Weather() {
             return resp.json()
         }).then(function (data) {
             //добавляем название города "lat":48.2864702,"lon":25.9376532
-            document.getElementById("weather__city").innerHTML = data.name + ', ' + Math.round(data.main.temp - 273) + '°  ' + data.weather[0]['description'] + "  давление:" + Math.round(data.main.pressure*0.750062) + " мм р. с."; // + "/" + data.main.grnd_level;
+            document.getElementById("weather__city").innerHTML = data.name + ' ' + Math.round(data.main.temp - 273) + '°, ' + data.weather[0]['description'] + ", давление:" + Math.round(data.main.pressure * 0.750062) + " мм р. с."; // + "/" + data.main.grnd_level;
             //data.main.temp содержит значение в Кельвинах, отнимаем от  273, чтобы получить значение в градусах Цельсия
             //Добавляем иконку погоды
             // document.getElementById('weather__icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
@@ -225,7 +232,7 @@ closer_point.id = 'closer_point';
 closer_point.innerText = 'X';
 
 
-startCalendar(2022);
+startCalendar(new Date().getFullYear());
 
 //--------------обработчики событий--------------------------------------
 
@@ -245,7 +252,7 @@ document.getElementsByClassName('weather')[0].addEventListener("click", function
     Weather()
 });
 document.getElementsByClassName('current_date')[0].addEventListener("click", function (event) {
-    document.getElementById('inputka').value = "2022";
+    document.getElementById('inputka').value = new Date().getFullYear();
     startCalendar(2022)
 }); //
 document.getElementById('inputka').oninput = function (event) {
